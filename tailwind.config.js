b09f2745272925ab87d314e4021b32e190e1ba16
Tridib Banik <tridib.perfect@gmail.com>
2026-07-19 00:07:08 -0400

    Add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}", // includes all components
+  ],
+  theme: {
+    extend: {}, // put your custom styles here
+  },
+  plugins: [],
+};
